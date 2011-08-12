a0cce49b101c06c5c8d06379d7ccf9f41895fd02
CatorgHans <hidink@orgis.nl>
2011-08-12 13:15:59 +0200

    different user on server and client

--- install.sh
+++ install.sh
@@ -52,8 +52,11 @@ questions(){
 	echo -n "> SERVER: SSH port: "
 	read port
 	
-	echo -n "> SERVER/CLIENT: username (must exist on both): "
-    	read username
+	echo -n "> SERVER: username: "
+    	read username_server
+
+	echo -n "> CLIENT: username: "
+	read username_client
     
 	echo -n "> CLIENT: directory to be synced: "
 	read lipsync_dir_local
@@ -63,14 +66,14 @@ questions(){
 }
 
 ssh.keygen(){
-  if ssh -i /home/${username}/.ssh/id_dsa -p ${port} -o "KbdInteractiveAuthentication=no" -o "PasswordAuthentication=no" ${username}@${remote_server} echo "hello" > /dev/null
+  if ssh -i /home/${username_client}/.ssh/id_dsa -p ${port} -o "KbdInteractiveAuthentication=no" -o "PasswordAuthentication=no" ${username_server}@${remote_server} echo "hello" > /dev/null
   then
     echo "	ssh key exists here and on server, skipping key generation and transfer steps"
     return
   else
-  	if [ -f '/home/${username}/.ssh/id_dsa' ]; then
-  		echo "* Existing SSH key found for ${username} backing up..."
-  		mv /home/${username}/.ssh/id_dsa /home/${username}/.ssh/id_dsa-OLD
+  	if [ -f '/home/${username_client}/.ssh/id_dsa' ]; then
+  		echo "* Existing SSH key found for ${username_client} backing up..."
+  		mv /home/${username_client}/.ssh/id_dsa /home/${username_client}/.ssh/id_dsa-OLD
   		if [ $? -eq 0 ]; then
   			echo "done"
   		else
@@ -78,24 +81,24 @@ ssh.keygen(){
   		fi
   	fi
   
-  	echo "* Checking for an SSH key for ${username}..."
-  	if [ -f /home/${username}/.ssh/id_dsa ]; then
+  	echo "* Checking for an SSH key for ${username_client}..."
+  	if [ -f /home/${username_client}/.ssh/id_dsa ]; then
   		echo "* Existing key found, not creating a new one..."
   	else
-  		echo -n "* No existing key found, creating SSH key for ${username}..."
-  		ssh-keygen -q -N '' -f /home/${username}/.ssh/id_dsa
+  		echo -n "* No existing key found, creating SSH key for ${username_client}..."
+  		ssh-keygen -q -N '' -f /home/${username_client}/.ssh/id_dsa
   		if [ $? -eq 0 ]; then
-  		chown -R $username:$username /home/${username}/.ssh
+  		chown -R $username_client:$username_client /home/$username_client/.ssh
   			echo "done"
   		else
   			echo; echo "	ERROR: there was an error generating the ssh key"; exit 1
   		fi
   	fi
   	
-  	echo "* Transferring ssh key for ${username} to ${remote_server} on port ${port} (login as $username now)..."; 
+  	echo "* Transferring ssh key for ${username_client} to ${remote_server} on port ${port} (login as $username_server now)..."; 
 
 	if which ssh-copy-id &> /dev/null; then
-  		su ${username} -c "ssh-copy-id -i /home/${username}/.ssh/id_dsa.pub '-p ${port} ${username}@${remote_server}'" >> /dev/null
+  		su ${username_client} -c "ssh-copy-id -i /home/${username_client}/.ssh/id_dsa.pub '-p ${port} ${username_server}@${remote_server}'" >> /dev/null
   		if [ $? -eq 0 ]; then
   			X=0	#echo "done"
   		else
@@ -104,7 +107,7 @@ ssh.keygen(){
 			exit 1 
 		fi
 	else
-  		su ${username} -c "cat /home/${username}/.ssh/id_rsa.pub | ssh $remote_server -p ${port} 'cat - > /home/${username}/.ssh/id_dsa.pub'" >> /dev/null
+  		su ${username_client} -c "cat /home/${username_client}/.ssh/id_rsa.pub | ssh ${username_server}@$remote_server -p ${port} 'cat - > /home/${username_server}/.ssh/id_dsa.pub'" >> /dev/null
   		if [ $? -eq 0 ]; then
   			X=0	#echo "done"
   		else
@@ -114,12 +117,12 @@ ssh.keygen(){
 		fi
 	fi
 
-  	echo -n "* Setting permissions on the ssh key for ${username} on ${remote_server} on port ${port}..."; 
-  	su ${username} -c "SSH_AUTH_SOCK=0 ssh ${remote_server} -p ${port} 'chmod 700 .ssh'"
+  	echo -n "* Setting permissions on the ssh key for ${username_server} on ${remote_server} on port ${port}..."; 
+  	su ${username_client} -c "SSH_AUTH_SOCK=0 ssh ${username_server}@${remote_server} -p ${port} 'chmod 700 .ssh'"
   	if [ $? -eq 0 ]; then
   		echo "done"
   	else
-  		echo; echo "	ERROR: there was an error setting permissions on the ssh key for ${username} on ${remote_server} on port ${port}..."; exit 1
+  		echo; echo "	ERROR: there was an error setting permissions on the ssh key for ${username_server} on ${remote_server} on port ${port}..."; exit 1
   	fi
   fi
 }
@@ -128,10 +131,11 @@ build.conf(){
 	echo -n "* Creating lipsyncd config..."
 	sed etc/lipsyncd_template > etc/lipsyncd \
 		-e 's|LSLOCDIR|'$lipsync_dir_local/'|g' \
-		-e 's|LSUSER|'$username'|g' \
+		-e 's|LSUSERCLIENT|'$username_client'|g' \
+		-e 's|LSUSERSERVER|'$username_server'|g' \
 		-e 's|LSPORT|'$port'|g' \
 		-e 's|LSREMSERV|'$remote_server'|g' \
-		-e 's|LSREMDIR|'$lipsync_dir_remote'|g'
+		-e 's|LSREMDIR|'$lipsync_dir_remote/'|g'
 	echo "done"
 }
 
@@ -152,9 +156,9 @@ deploy(){
 	cp etc/init.d/lipsyncd /etc/init.d
 	echo "done"
 
-	echo -n "	> Installing cron for user $username..."
+	echo -n "	> Installing cron for user $username_client..."
 	newcronjob="* * * * *  /usr/local/bin/lipsync >/dev/null 2>&1"      #define entry for crontab	
-	(crontab -u $username -l; echo "$newcronjob") | crontab -u $username - #list crontab, read entry from crontab, add line from stdin to crontab
+	(crontab -u $username_client -l; echo "$newcronjob") | crontab -u $username_client - #list crontab, read entry from crontab, add line from stdin to crontab
 	echo "done"
 
 	echo -n "	> /etc/lipsyncd..."
@@ -168,46 +172,46 @@ deploy(){
 	cp README* LICENSE uninstall.sh docs/* /usr/share/doc/lipsyncd
 	echo "done"
 
-	echo -n "	> /home/$username/.lipsyncd..."
- 	if [ ! -d /home/$username/.lipsyncd ]; then
-        	mkdir /home/$username/.lipsyncd
-		chown $username:$username /home/$username/.lipsyncd
+	echo -n "	> /home/$username_client/.lipsyncd..."
+ 	if [ ! -d /home/$username_client/.lipsyncd ]; then
+        	mkdir /home/$username_client/.lipsyncd
+		chown $username_client:$username_client /home/$username_client/.lipsyncd
         fi
 	echo "done"
 
-	echo -n "	> /home/$username/.lipsyncd/lipsyncd.log..."
-	touch /home/$username/.lipsyncd/lipsyncd.log
-	chown $username:$username /home/$username/.lipsyncd/lipsyncd.log
-	chmod g+w /home/$username/.lipsyncd/lipsyncd.log
+	echo -n "	> /home/$username_client/.lipsyncd/lipsyncd.log..."
+	touch /home/$username_client/.lipsyncd/lipsyncd.log
+	chown $username_client:$username_client /home/$username_client/.lipsyncd/lipsyncd.log
+	chmod g+w /home/$username_client/.lipsyncd/lipsyncd.log
 	echo "done"
 
 	echo -n "	> checking for $lipsync_dir_local..."
 	if [ ! -d $lipsync_dir_local ]; then
 		echo; echo -n "	> $lipsync_dir_local not found, creating..."
 		mkdir $lipsync_dir_local
-		chown $username:$username $lipsync_dir_local
+		chown $username_client:$username_client $lipsync_dir_local
 	fi
 	echo "done"
 
-	echo "lipsync installed `date`" > /home/$username/.lipsyncd/lipsyncd.log
+	echo "lipsync installed `date`" > /home/$username_client/.lipsyncd/lipsyncd.log
 }
 
 initial_sync(){
 	echo -n "* Doing inital sync with server..."
 	. /etc/lipsyncd
-	su $USER_NAME -c 'rsync -rav --stats --log-file=/home/'$USER_NAME'/.lipsyncd/lipsyncd.log -e "ssh -l '$USER_NAME' -p '$SSH_PORT'" '$REMOTE_HOST':'$LOCAL_DIR' '$REMOTE_DIR''
-	echo "Initial sync `date` Completed" > /home/$username/.lipsyncd/lipsyncd.log
+	su $USER_NAME_CLIENT -c 'rsync -rav --stats --log-file=/home/'$USER_NAME_CLIENT'/.lipsyncd/lipsyncd.log -e "ssh -l '$USER_NAME_SERVER' -p '$SSH_PORT'" '$REMOTE_HOST':'$REMOTE_DIR' '$LOCAL_DIR''
+	echo "Initial sync `date` Completed" > /home/$username_client/.lipsyncd/lipsyncd.log
 }
 
 start(){
 	/etc/init.d/lipsyncd start; sleep 2
-	if [ -f /home/$username/.lipsyncd/lipsyncd.pid ]; then
-#		echo "	NOTICE: lipsyncd is running as pid `cat /home/$username/.lipsyncd/lipsyncd.pid`"
+	if [ -f /home/$username_client/.lipsyncd/lipsyncd.pid ]; then
+#		echo "	NOTICE: lipsyncd is running as pid `cat /home/$username_client/.lipsyncd/lipsyncd.pid`"
 		echo "	NOTICE: lipsyncd is running as pid `pidof lipsyncd`"
 		echo "	Check lipsyncd.log for details"
 	else
 		echo "	NOTICE: lipsyncd failed to start..."
-		echo "	Check /home/$username/.lipsyncd/lipsyncd.log for details"
+		echo "	Check /home/$username_client/.lipsyncd/lipsyncd.log for details"
 	fi
 }
 
